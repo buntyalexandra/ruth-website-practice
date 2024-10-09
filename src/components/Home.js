718c5b399767d090a488b6eb8fdd38626f3fb60b
Alexandra Bunty <buntyalexandra@gmail.com>
2024-10-09 10:33:06 -0400

    set background image on Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,17 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
 import backgroundImg from "../assets/strawb_1.jpg";
 
 const Home = () => {
   return (
-    <Container
-      fluid
-      className="bg-image object-fit-contain vw-100 vh-100"
-      style={{ backgroundImage: `url(${backgroundImg})` }}
+    <div
+      className="vw-100 vh-100"
+      style={{
+        backgroundImage: `url(${backgroundImg})`,
+        backgroundSize: "cover",
+      }}
     >
-      <h1>HOME</h1>
-    </Container>
+      <h1>ARIELLA RUTH ADELSTIEN</h1>
+    </div>
   );
 };
 export default Home;
